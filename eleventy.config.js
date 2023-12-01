d0f000dfb517588fbb62bb56677ca8a67ed9a37a
Benjamin Abel <dev.abel@free.fr>
2023-12-01 18:06:09 +0100

    Format with prettier

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,36 +1,29 @@
 // need to bundle it with esbuild until 11ty supports esm
 const md2HTML = require("./scripts/build/md2HTML.cjs").md2HTML;
 
-
-
-
-
 module.exports = function (eleventyConfig) {
+  // Add emd as a valid extension to process
+  eleventyConfig.addExtension(["emd"], {
+    key: "html",
+    compile: function (inputContent, inputPath) {
+      return function (data) {
+        return md2HTML(inputContent);
+        // TODO maybe preprocess html at build time
+        // return this.defaultRenderer(data);
+      };
+    },
+  });
+  // watch for dev
+  eleventyConfig.addWatchTarget("11ty/_assets/style.css");
+  // copy assets to oudir
+  eleventyConfig.addPassthroughCopy({ "design/*.svg": "." });
+  eleventyConfig.addPassthroughCopy({ "11ty/_assets/style.css": "." });
 
-    // Add emd as a valid extension to process
-    eleventyConfig.addExtension(['emd'], {
-        key: 'html',
-        compile: function (inputContent, inputPath) {
-            return function (data) {
-                return md2HTML(inputContent)
-                // TODO maybe preprocess html at build time
-                // return this.defaultRenderer(data);
-            };
-        }
-    }
-    )
-    // watch for dev
-    eleventyConfig.addWatchTarget("11ty/_assets/style.css");
-    // copy assets to oudir
-    eleventyConfig.addPassthroughCopy({ "design/*.svg": "." });
-    eleventyConfig.addPassthroughCopy({ "11ty/_assets/style.css": "." });
-
-    return {
-        dir: {
-            input: "content",
-            data: "../11ty/_data",
-            includes: "../11ty/_includes"
-        }
-    }
+  return {
+    dir: {
+      input: "content",
+      data: "../11ty/_data",
+      includes: "../11ty/_includes",
+    },
+  };
 };
-
